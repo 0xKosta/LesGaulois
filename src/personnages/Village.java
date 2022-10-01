b2c8fe6447de2f076a2686c996b651fd385bb90f
K <konstantin.andjelkovic03@gmail.com>
2022-10-01 22:36:54 +0200

    Update by Kosta

---
+++ src/personnages/Village.java
@@ -0,0 +1,16 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	private int population;
+	
+	public Village(String nom, int population) {
+		super();
+		this.nom = nom;
+		this.population = population;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+}

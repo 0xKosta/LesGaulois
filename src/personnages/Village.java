6270e6a1dbe9a6326f2d263f7fcce15b65d4b2d9
K <konstantin.andjelkovic03@gmail.com>
2022-10-05 14:15:40 +0200

    Mise a jour par K.

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,28 +2,61 @@ package personnages;
 
 public class Village {
 	private String nom;
-	private int population;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private Chef chef;
 	
-	public Village(String nom, int population) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
-		this.population = population;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void afficherVillageois() {
+		int i = 0;
+		String annonce = "Dans village du chef " + chef.getNom() + 
+				" vivent les legendaires gaulois:";
+		System.out.println(annonce);
+		while (villageois[i] != null) {
+			System.out.println("- " + villageois[i].getNom());
+			i++;
+		}
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;	
+	}
+	
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois]; 
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 
 	public String getNom() {
 		return nom;
 	}
 	
-	public void afficherPopulation() {
-		System.out.println("Le village de " + nom + " a une population de " + population + ".");
-	}
-	
-	public void ajouterHabitant() {
-		population += 1;
-	}
 	
 	public static void main(String args[]) {
-		Village amorica = new Village("Amorica", 10);
-		amorica.afficherPopulation();
+		Village village = new Village("Village des Irreductibles", 30);
+		//Gaulois gaulois = village.trouverHabitant(30);
+		//Les tableaux en Java ont comme premier indice 0, 
+		//donc un tableau de longueur 30 va de 0 a 29.
+		Chef abracourcix = new Chef("Abracourcix", 6, village);
+		village.setChef(abracourcix);
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		village.ajouterHabitant(asterix);
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//La reponse obtenue est null car un villageois avec le indice 1 n'existe pas
+		//et donc il n'y a aucune information sur ce gaulois.
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
+		
 	}
 }
 

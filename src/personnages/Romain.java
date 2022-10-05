2394da30c0bae290f7c5956ff32df81e36c282ea
K <konstantin.andjelkovic03@gmail.com>
2022-10-05 14:51:05 +0200

    Mise a jour par K.

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		assert (force > 0);
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 
 	public String getNom() {
@@ -33,10 +36,12 @@ public class Romain {
 	
 	public static void main(String[] args) {
 		//TODO creer un main qui permet de tester la classe Romain.
-		Romain cesar = new Romain("Cesar", -6);
+		Romain cesar = new Romain("Cesar", 10);
 		System.out.println(cesar.prendreParole());
 		cesar.parler("Bonjour");
 		cesar.recevoirCoup(10);
 		cesar.recevoirCoup(10);
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
 	}
 }
